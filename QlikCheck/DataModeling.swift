d14534e2e659c0796307ad3610a192fed6ecc628
RohitKanade <kanade3@gmail.com>
2017-10-15 19:48:45 +0530

    Added UI for questions

--- QlikCheck/DataModeling.swift
+++ QlikCheck/DataModeling.swift
@@ -19,14 +19,68 @@ func DataModeling() -> [Questions] {
     // 1 
     let one = Questions()
     one.question = "testing question 1 "
+    one.optionA = "option A"
+    one.optionB = "option B"
+    one.optionC = "option C"
+    one.optionD = "option D"
+    
+    one.explanation = "Its Explaination"
+    one.rightAnswer = "optionA"
     
     questions.append(one)
     
     // 2
     let two = Questions()
     two.question = "testing question 2 "
+    two.optionA = "option A"
+    two.optionB = "option B"
+    two.optionC = "option C"
+    two.optionD = "option D"
+
+    two.explanation = "Its Explaination"
+    two.rightAnswer = "optionA"
 
     questions.append(two)
+    
+    // 3
+    let three = Questions()
+    three.question = "testing question 3 "
+    three.optionA = "option A"
+    three.optionB = "option B"
+    three.optionC = "option C"
+    three.optionD = "option D"
+
+    three.explanation = "Its Explaination"
+    three.rightAnswer = "optionA"
+    
+    questions.append(three)
+    
+    // 4
+    let four = Questions()
+    four.question = "testing question 4 "
+    four.optionA = "option A"
+    four.optionB = "option B"
+    four.optionC = "option C"
+    four.optionD = "option D"
+
+    four.explanation = "Its Explaination"
+    four.rightAnswer = "optionA"
+    
+    questions.append(four)
+    
+    // 5
+    let five = Questions()
+    five.question = "testing question 5 "
+    five.optionA = "option A"
+    five.optionB = "option B"
+    five.optionC = "option C"
+    five.optionD = "option D"
+
+    five.explanation = "Its Explaination"
+    five.rightAnswer = "optionA"
+    
+    questions.append(five)
+    
     
     return questions
     
--- QlikCheck/QuestionViewController.swift
+++ QlikCheck/QuestionViewController.swift
@@ -9,30 +9,92 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    
+    @IBOutlet weak var counter: UILabel!
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var optionA: UIButton!
+    @IBOutlet weak var optionB: UIButton!
+    @IBOutlet weak var optionC: UIButton!
+    @IBOutlet weak var optionD: UIButton!
+    
+    var totalQuestion = 0, currentCount = 0
 
     var questions = [Questions]()
+    var currentQuestion:Questions? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(questions)
-        // Do any additional setup after loading the view.
+        
+        totalQuestion = questions.count
+        currentQuestion = questions[0]
+
+        
+        setQuestion(currentQuestion: currentQuestion!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setQuestion(currentQuestion : Questions)  {
+        counter.text = "Questions \(currentCount+1) of \(totalQuestion)"
+        question.text = "\(String(describing: currentQuestion.question!))"
+        optionA.setTitle("A: \(String(describing: currentQuestion.optionA!))", for: .normal)
+        optionB.setTitle("B: \(String(describing: currentQuestion.optionB!))", for: .normal)
+        optionC.setTitle("C: \(String(describing: currentQuestion.optionC!))", for: .normal)
+        optionD.setTitle("D: \(String(describing: currentQuestion.optionD!))", for: .normal)
     }
-    */
+    
+    
+    // Answer selected
+    @IBAction func OptionASelected(_ sender: Any) {
+        optionA.backgroundColor = UIColor.cyan
+        
+        optionB.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+        optionC.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+        optionD.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+    }
 
+    @IBAction func OptionBSelected(_ sender: Any) {
+        optionB.backgroundColor = UIColor.cyan
+        
+        optionA.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+        optionC.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+        optionD.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+    }
+    
+    @IBAction func OptionCSelected(_ sender: Any) {
+        optionC.backgroundColor = UIColor.cyan
+        
+        optionA.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+        optionB.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+        optionD.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+    }
+
+    @IBAction func OptionDSelected(_ sender: Any) {
+        optionD.backgroundColor = UIColor.cyan
+        
+        optionB.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+        optionC.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+        optionA.backgroundColor = hexStringToUIColor(hex: "F9F9F9")
+    }
+    
+    
+    // Show Answer
+    @IBAction func ShowAnswer(_ sender: Any) {
+    }
+    
+    // Next
+    @IBAction func NextSelected(_ sender: Any) {
+        currentCount = currentCount+1
+        if currentCount == totalQuestion{
+            
+        }else{
+            currentQuestion = questions[currentCount]
+            setQuestion(currentQuestion: currentQuestion!)
+        }
+    }
 }
